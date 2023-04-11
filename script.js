66d5a296a0c78670b47ead35c84866934c5e0d18
GorbunovSergey <anabiosis56@yandex.ru>
2023-04-11 23:23:10 +0300

    calculator

---
+++ script.js
@@ -0,0 +1,20 @@
+let input = document.querySelector('.enter');
+let nums = document.querySelectorAll('.num');
+let result = document.querySelector('.result');
+let clear = document.querySelector('.clear');
+for (let num of nums) {
+        num.addEventListener('click', function() {
+
+            input.value += num.textContent;
+        });
+    };
+
+clear.addEventListener('click', function() {
+    input.value = ' ';
+});    
+result.addEventListener('click', function() {
+
+    input.value = eval(input.value);
+});
+
+
