b0fb988ccd14430d1e5ab1f71540056f3129d0c1
GorbunovSergey <anabiosis56@yandex.ru>
2023-04-12 15:26:36 +0300

    add history and clear history buttons and functions, fixed formating.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-let input = document.querySelector('.enter');
-let nums = document.querySelectorAll('.num');
-let result = document.querySelector('.result');
-let clear = document.querySelector('.clear');
+const input = document.querySelector(".enter");
+const nums = document.querySelectorAll(".num");
+const result = document.querySelector(".result");
+const clear = document.querySelector(".clear");
+const history = document.querySelector(".history");
+const btnHistory = document.querySelector(".btnHistory");
+const clearHistory = document.querySelector(".clearHistory");
+
 for (let num of nums) {
-        num.addEventListener('click', function() {
+  num.addEventListener("click", function () {
+    input.value += num.textContent;
+  });
+};
 
-            input.value += num.textContent;
-        });
-    };
-
-clear.addEventListener('click', function() {
-    input.value = ' ';
-});    
-result.addEventListener('click', function() {
-
-    input.value = eval(input.value);
+clear.addEventListener("click", function () {
+  input.value = " ";
 });
-                                                                                                    id_rsa.pub                                                                                                         
 
+let count = 0;
+result.addEventListener("click", function () {
+  let his = eval(input.value);
+  input.value = his;
+  localStorage.setItem(count, his);
+  count++;
+});
 
+btnHistory.addEventListener("click", function () {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    history.value += localStorage.getItem(key);
+  }
+});
+
+clearHistory.addEventListener("click", function () {
+  localStorage.clear();
+  history.value = "";
+});
